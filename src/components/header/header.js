8346249cacaf011bd5e986a3f0958f52f72f9489
MartaGreen <iguana13131@gmail.com>
2022-07-30 12:32:58 +0400

    feat: start creation of screen adaption by creating sidebar

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,3 +1,29 @@
 import "./header.scss";
+import "./sidebar.scss";
 
 import "../../assets/images/logo.svg";
+
+window.onload = () => {
+  const sidebar = document.getElementById("sidebar");
+  const sidebarBurger = sidebar.querySelector(".sidebar__burger");
+  const sidebarContent = sidebar.querySelector(".sidebar__content");
+  let isOpened = false;
+
+  const openSidebar = () => {
+    sidebarBurger.classList.add("sidebar__burger_open");
+    sidebarBurger.classList.remove("sidebar__burger_close");
+    sidebarContent.classList.remove("sidebar__content_hidden");
+  };
+  const closeSidebar = () => {
+    sidebarBurger.classList.remove("sidebar__burger_open");
+    sidebarBurger.classList.add("sidebar__burger_close");
+    sidebarContent.classList.add("sidebar__content_hidden");
+  };
+
+  sidebarBurger.onclick = () => {
+    if (isOpened) closeSidebar();
+    if (!isOpened) openSidebar();
+
+    isOpened = !isOpened;
+  };
+};
